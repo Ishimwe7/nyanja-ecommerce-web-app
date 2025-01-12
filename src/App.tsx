8ea5b42a851b3fb1a5625aa7d54f9cfa15ef3b12
Ishimwe Pacifique <101452809+Ishimwe7@users.noreply.github.com>
2025-01-12 10:35:34 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-//import { useState } from 'react'
-//import reactLogo from './assets/react.svg'
-//import viteLogo from '/vite.svg'
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RegistrationForm from './components/registration';
 import LoginForm from './components/loginForm';
@@ -8,8 +6,6 @@ import Home from './components/home';
 import AdminDashoard from './components/adminDashboard';
 import JewelleriesProducts from './components/Jewelleries';
 import Checkout from './components/Checkout';
-// import Cart from './components/Cart';
-// import Orders from './components/Orders';
 function App() {
   //const [count, setCount] = useState(0)
   return (
